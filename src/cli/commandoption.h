f99f1584044a5f56e165efa86219fd1f150e784a
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-29 07:34:44 +0100

    added comments Update commandoption.h
    These comments provide an overview of the C++ class `CommandOption ` and explain the purpose and usage of its member functions and variables.

--- src/cli/commandoption.h
+++ src/cli/commandoption.h
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: 2017-2019 Alejandro Sirgo Rica & Contributors
-
 #pragma once
 
 #include <QStringList>
@@ -8,9 +7,11 @@
 
 using std::function;
 
+// Class representing a command-line option
 class CommandOption
 {
 public:
+    // Constructors for creating a CommandOption
     CommandOption(const QString& name,
                   QString description,
                   QString valueName = QString(),
@@ -44,11 +45,11 @@ public:
     bool operator==(const CommandOption& option) const;
 
 private:
-    QStringList m_names;
-    QString m_description;
-    QString m_valueName;
-    QString m_value;
+    QStringList m_names;        // Names for the option
+    QString m_description;      // Description of the option
+    QString m_valueName;       // Name of the option's value
+    QString m_value;           // The actual value
 
-    function<bool(QString const&)> m_checker;
-    QString m_errorMsg;
+    function<bool(QString const&)> m_checker; // Value checker function
+    QString m_errorMsg;        // Error message for value checks
 };
